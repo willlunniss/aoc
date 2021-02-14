58c1aa455576b79273d94fbf44df0c4f1894575b
Will Lunniss <willlunniss@gmail.com>
2021-02-14 16:35:10 +0000

    Simplify day 4 & 6 solutions

--- 2020/src/solutions/day4_passport_processing.rs
+++ 2020/src/solutions/day4_passport_processing.rs
@@ -1,5 +1,6 @@
 use std::collections::HashMap;
 use regex::Regex;
+use itertools::Itertools;
 use lazy_static::lazy_static;
 
 lazy_static! {
@@ -9,114 +10,91 @@ lazy_static! {
     static ref RE_PID: Regex = Regex::new(r"^[0-9]{9}$").unwrap();
 }
 
-fn validate_passport_data(data: &HashMap<&str, &str>) -> (bool, bool) {
+fn has_required(data: &HashMap<&str, &str>) -> bool {
     // Check all required fields (don't requir cid)
     let required_fields = ["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"];
     for required in required_fields.iter() {
         if !data.contains_key(required) {
-            return (false, false);
+            return false;
         }
     }
-    // Now that we know they are all there for the second part check they are in range
-    // byr (Birth Year) - four digits; at least 1920 and at most 2002.
-    let byr = data.get("byr").unwrap().parse::<usize>().unwrap();
-    if byr < 1920 || byr > 2002 { return (true, false); }
-    // iyr (Issue Year) - four digits; at least 2010 and at most 2020.
-    let iyr = data.get("iyr").unwrap().parse::<usize>().unwrap();
-    if iyr < 2010 || iyr > 2020 { return (true, false); }
-    // eyr (Expiration Year) - four digits; at least 2020 and at most 2030.
-    let eyr = data.get("eyr").unwrap().parse::<usize>().unwrap();
-    if eyr < 2020 || eyr > 2030 { return (true, false); }
-    // hgt (Height) - a number followed by either cm or in:
-    // If cm, the number must be at least 150 and at most 193.
-    // If in, the number must be at least 59 and at most 76.
-    let capture = RE_HGT.captures(data.get("hgt").unwrap());
-    if capture.is_none() { return (true, false); } // Doesn't match
-    let hgt = capture.unwrap();
-    let hgt_value = hgt.get(1).unwrap().as_str().parse::<usize>().unwrap();
-    if hgt.get(2).unwrap().as_str() == "cm" {
-        if hgt_value < 150 || hgt_value > 193 { return (true, false); }
-    } else if hgt.get(2).unwrap().as_str() == "in" {
-        if hgt_value < 59 || hgt_value > 76 { return (true, false); }
-    }    
-    // hcl (Hair Color) - a # followed by exactly six characters 0-9 or a-f.
-    let matches = RE_HCL.captures(data.get("hcl").unwrap());
-    if matches.is_none() { return (true, false); } // Doesn't match
-
-    // ecl (Eye Color) - exactly one of: amb blu brn gry grn hzl oth.
-    let ecl = *data.get("ecl").unwrap();
-    if ecl != "amb" && ecl != "blu" && ecl != "brn" && ecl != "gry" && ecl != "grn" && ecl != "hzl" && ecl != "oth" { return (true, false); } // Doesn't match
-
-    // pid (Passport ID) - a nine-digit number, including leading zeroes.
-    let matches = RE_PID.captures(data.get("pid").unwrap());
-    if matches.is_none() { return (true, false); } // Doesn't match
-
-    // cid (Country ID) - ignored, missing or not.
-
-    return (true, true);
+    return true;
 }
 
+fn is_valid(key: &str, value: &str) -> bool {
+    return match key {
+        "byr" => {
+            // byr (Birth Year) - four digits; at least 1920 and at most 2002.
+            (1920..=2002).contains(&value.parse::<usize>().unwrap())
+        },
+        "iyr" => {
+            // iyr (Issue Year) - four digits; at least 2010 and at most 2020.
+            (2010..=2020).contains(&value.parse::<usize>().unwrap())
+        },
+        "eyr" => {
+            // eyr (Expiration Year) - four digits; at least 2020 and at most 2030.
+            (2020..=2030).contains(&value.parse::<usize>().unwrap())
+        },
+        "hgt" => {
+            // hgt (Height) - a number followed by either cm or in:
+            // If cm, the number must be at least 150 and at most 193.
+            // If in, the number must be at least 59 and at most 76.
+            let capture = RE_HGT.captures(value);
+            if capture.is_none() { 
+                false
+            } else {
+                let hgt = capture.unwrap();
+                if hgt.get(2).unwrap().as_str() == "cm" {
+                    (150..=193).contains(&hgt.get(1).unwrap().as_str().parse::<usize>().unwrap())
+                } else if hgt.get(2).unwrap().as_str() == "in" {
+                    (59..=76).contains(&hgt.get(1).unwrap().as_str().parse::<usize>().unwrap())
+                } else {
+                    false
+                }
+            }
+        },
+        "hcl" => {
+            // hcl (Hair Color) - a # followed by exactly six characters 0-9 or a-f.
+            RE_HCL.is_match(value)
+        },
+        "ecl" => {
+            // ecl (Eye Color) - exactly one of: amb blu brn gry grn hzl oth.
+            ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"].contains(&value)
+        },
+        "pid" => {
+            // pid (Passport ID) - a nine-digit number, including leading zeroes.
+            RE_PID.is_match(value)
+        },
+        _ => { true } // Don't care about other fields
+    }
+}
+
+fn all_valid(data: &HashMap<&str, &str>) -> bool {
+    // Check all required fields (don't requir cid)
+    if !has_required(data) {
+        return false;
+    }
+    // Now that we know they are all there for the second part check they are in range
+    for (key, value) in data.iter() {
+        if !is_valid(key, value) { return false; }
+    }
+    return true;
+}
+
+pub fn gen(input: &str) -> Vec<HashMap<&str, &str>> {
+    return input.split("\r\n\r\n") // Split into sections based on empty lines
+        .map(|section| section.split_whitespace().map(|field| field.splitn(2, ':').collect_tuple().unwrap()).collect()) // Build HashMap of key/value
+        .collect(); // Build a Vector of the HashMaps
+}
 
 #[aoc(day4, part1)]
 fn part1(input: &str) -> usize {
-    // Process the passport data in key:value pairs seperated by spaces and new lines
-    // A blank line indicates start of a new passport
-    let mut complete = 0;
-    let mut data : HashMap<&str, &str> = HashMap::new();
-    for line in input.lines() {
-        if line.is_empty() {
-            // End of current passport, validate
-            let (fields_present, _) = validate_passport_data(&data);
-            if fields_present {
-                complete += 1;
-            }
-            data.clear();
-            continue;
-        }
-        // Each row may have multiple fields
-        let fields = line.split(' ');
-        for field in fields {
-            // Split out key/pair values and add to data hash
-            let parts : Vec<&str> = field.split(':').collect();
-            data.insert(parts[0], parts[1]);
-        }
-    }
-    // Check any non-terminated data at the end
-    let (fields_present, _) = validate_passport_data(&data);
-    if fields_present {
-        complete += 1;
-    }
-    return complete;
+    // Count number of passports that have all the required fields
+    return gen(input).iter().filter(|&passport| has_required(passport)).count();
 }
 
 #[aoc(day4, part2)]
-fn part2(input: &str) -> usize {    
-    // Process the passport data in key:value pairs seperated by spaces and new lines
-    // A blank line indicates start of a new passport
-    let mut valid = 0;
-    let mut data : HashMap<&str, &str> = HashMap::new();
-    for line in input.lines() {
-        if line.is_empty() {
-            // End of current passport, validate
-            let (_, fields_valid) = validate_passport_data(&data);
-            if fields_valid {
-                valid += 1;
-            }
-            data.clear();
-            continue;
-        }
-        // Each row may have multiple fields
-        let fields = line.split(' ');
-        for field in fields {
-            // Split out key/pair values and add to data hash
-            let parts : Vec<&str> = field.split(':').collect();
-            data.insert(parts[0], parts[1]);
-        }
-    }
-    // Check any non-terminated data at the end
-    let (_, fields_valid) = validate_passport_data(&data);
-    if fields_valid {
-        valid += 1;
-    }
-    return valid;
+fn part2(input: &str) -> usize {
+    // Count number of passports where all fields are valid
+    return gen(input).iter().filter(|&passport| all_valid(passport)).count();
 }
--- 2020/src/solutions/day6_custom_customs.rs
+++ 2020/src/solutions/day6_custom_customs.rs
@@ -1,59 +1,32 @@
-use std::collections::HashMap;
 use std::collections::HashSet;
 
 #[aoc(day6, part1)]
 fn part1(input: &str) -> usize {
-     // Process the questions, each group is seperate by a blank line
-     let mut questions : HashSet<char> = HashSet::new();
-     let mut question_count_sum = 0;
-     for line in input.lines() {
-         if line.is_empty() {
-             // Blank line - end of group
-             question_count_sum += questions.len();
-             // Reset for next
-             questions.clear();
-             continue;
-         }
-         for q in line.chars() {
-             // Increment the counter for the question
-             questions.insert(q);
-         }
-     }
-     if questions.len() > 0 {        
-         // Process final group
-         // Total questions answered yes to
-         question_count_sum += questions.len();
-     }
-     return question_count_sum;
+    // Split into groups based on empty lines
+    // Count number of uniq non-whitespace chars for each group by putting them in a HashSet and getting the length
+    // Sum across all groups
+    return input.split("\r\n\r\n")
+        .map(|group| group.chars().filter(|c| !c.is_whitespace()).collect::<HashSet<char>>().len())
+        .sum();
 }
 
 #[aoc(day6, part2)]
 fn part2(input: &str) -> usize {
-     // Process the questions, each group is seperate by a blank line
-     let mut questions : HashMap<char, usize> = HashMap::new();
-     let mut question_all_sum = 0;
-     let mut group_size = 0;
-     for line in input.lines() {
-         if line.is_empty() {
-             // Blank line - end of group
-             // Count questions where everyone answered yes (responses == group size)
-             question_all_sum += questions.values().filter(|&responses| *responses == group_size).count();
-             // Reset for next
-             questions.clear();
-             group_size = 0;
-             continue;
-         }
-          // Increment size of this group and then process this persons question
-         group_size += 1;
-         for q in line.chars() {
-             // Increment the counter for the question
-             questions.insert(q, 1 + if questions.contains_key(&q) { questions[&q] } else { 0 });
-         }
-     }
-     if questions.keys().len() > 0 {        
-         // Process final group
-         // Questions where everyone answered yes
-         question_all_sum += questions.values().filter(|&n| *n == group_size).count();
-     }
-     return question_all_sum;
+    // Split into groups based on empty lines
+    // For each line (person's answers) turn into HashSet of chars
+    // Find the intersection of each persons answers for that group and get length
+    // Sum across all groups    
+    return input.split("\r\n\r\n")
+        .map(|group| {
+            group.lines().map(|line| line.chars().collect())
+                .fold(None, | acc: Option<HashSet<char>>, chars| {
+                    if acc == None {
+                        Some(chars) // Initialise with first persons answers
+                    } else {
+                        // For each subsequent person reduce by calculating the intersection
+                        Some(acc.unwrap().intersection(&chars).copied().collect())
+                    }
+                }).unwrap().len()
+        })
+        .sum();
 }

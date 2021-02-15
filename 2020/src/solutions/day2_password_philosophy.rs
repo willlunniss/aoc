5018d8eae811d1b84429cabdb9230691ba33f071
Will Lunniss <willlunniss@gmail.com>
2021-02-15 14:01:25 +0000

    Simplify day 2 solution

--- 2020/src/solutions/day2_password_philosophy.rs
+++ 2020/src/solutions/day2_password_philosophy.rs
@@ -1,60 +1,62 @@
+use itertools::Itertools;
+use std::convert::Infallible;
+use std::str::FromStr;
+
+pub struct PasswordPolicy {
+    password: String,
+    policy_char: char,
+    val1: usize,
+    val2: usize,
+}
+
+impl FromStr for PasswordPolicy {
+    type Err = Infallible;
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        // Decode the password and polcy of form
+        // 1-3 a: abcde
+        let (policy, password) = s.splitn(2, ": ").collect_tuple().unwrap();
+        let (val1, val2, policy_char) = policy.splitn(3, &['-', ' '][..]).collect_tuple().unwrap();
+        return Ok(PasswordPolicy {
+            password: password.to_string(),
+            policy_char: policy_char.chars().nth(0).unwrap(),
+            val1: val1.parse().unwrap(),
+            val2: val2.parse().unwrap(),
+        });
+    }
+}
+
+#[aoc_generator(day2)]
+pub fn gen(input: &str) -> Vec<PasswordPolicy> {
+    input.lines().map(|x| x.parse().unwrap()).collect()
+}
+
 #[aoc(day2, part1)]
-fn part1(input: &str) -> usize {
-    let mut valid = 0;
+fn part1(input: &Vec<PasswordPolicy>) -> usize {
     // Check each entry for validity and count the number that are valid
-    /* 
-    Each line gives the password policy and then the password. 
+    /*
     The password policy indicates the lowest and highest number of times a given letter must appear for the password to be valid.
     For example, 1-3 a means that the password must contain a at least 1 time and at most 3 times.
-    */ 
-    for s in input.lines() {
-        let parts : Vec<&str> = s.split(": ").collect();
-        let password = parts[1]; // Get the password
-        let policy = parts[0]; // Extrat the policy fields (can't assume single digits)
-        let policy_parts : Vec<&str> = policy.split(&['-', ' '][..]).collect();
-        let min = policy_parts[0].parse::<i64>().unwrap();
-        let max = policy_parts[1].parse::<i64>().unwrap();
-        let char = policy_parts[2];
-        //println!("Checking {} against {}:{}/{}", password, char, min, max);
-        let occurrences = password.matches(char).count() as i64;
-        // Check we have the right number of occurrences of the policy char
-        if occurrences >= min && occurrences <= max {
-           valid += 1;
-        }
-    }
-    return valid;
+    */
+    return input.iter()
+        .filter(|p| {
+            (p.val1..=p.val2).contains(&p.password.matches(p.policy_char).count())
+        })
+        .collect::<Vec<_>>()
+        .len();
 }
 
 #[aoc(day2, part2)]
-fn part2(input: &str) -> usize {
-    let mut valid = 0;
+fn part2(input: &Vec<PasswordPolicy>) -> usize {
     // Check each entry for validity and count the number that are valid
-    /* 
+    /*
     Each policy actually describes two positions in the password, where 1 means the first character, 2 means the second character, and so on.
     (Be careful; Toboggan Corporate Policies have no concept of "index zero"!) Exactly one of these positions must contain the given letter.
     Other occurrences of the letter are irrelevant for the purposes of policy enforcement.
-    */ 
-    for s in input.lines() {
-        let parts : Vec<&str> = s.split(": ").collect();
-        let password = parts[1]; // Get the password
-        let policy = parts[0]; // Extrat the policy fields (can't assume single digits)
-        let policy_parts : Vec<&str> = policy.split(&['-', ' '][..]).collect();
-        // Get positions (and convert from 1 (puzzle input) to 0 (rust) indexing)
-        let pos1 = policy_parts[0].parse::<usize>().unwrap() - 1;
-        let pos2 = policy_parts[1].parse::<usize>().unwrap() - 1;
-        let char = policy_parts[2].chars().next();
-        // Check we have exactly 1 instance of char at the possitions pos1/pos2
-        let mut matches = 0;
-        if password.chars().nth(pos1) == char {
-            matches += 1;
-        }
-        if password.chars().nth(pos2) == char {
-           matches += 1;
-       }
-       if matches == 1 {
-           // Found just 1 match, password is valid
-           valid += 1;
-       }
-    }
-    return valid;
+    */
+    return input.iter()
+        .filter(|p| {
+            (p.password.chars().nth(p.val1-1).unwrap() == p.policy_char) ^ (p.password.chars().nth(p.val2-1).unwrap() == p.policy_char) 
+        })
+        .collect::<Vec<_>>()
+        .len();
 }

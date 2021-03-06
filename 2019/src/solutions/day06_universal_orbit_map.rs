b6535edeb9f1a742b38f2e1cf516c6938d74b763
Will Lunniss <willlunniss@gmail.com>
2021-03-06 11:53:18 +0000

    Solution for 2019 day 6

---
+++ 2019/src/solutions/day06_universal_orbit_map.rs
@@ -0,0 +1,48 @@
+use std::collections::HashMap;
+use itertools::Itertools;
+
+/// Walks the path from object -> COM
+fn walk<'a>(orbit_map: &'a HashMap<&str, &str>, object: &'a str) -> Vec<&'a str> {
+    let mut path = Vec::new();
+    let mut node = object;
+    // Keep resolving until there isn't a parent
+    while let Some(parent) = orbit_map.get(node) {
+        node = parent;
+        path.push(node);
+    }
+    return path;
+}
+
+fn gen(input: &str) -> HashMap<&str, &str> {
+    return input.lines().map(|line| line.splitn(2, ")").collect_tuple().unwrap()).map(|(parent, child)|(child, parent)).collect();
+}
+
+#[aoc(day6, part1)]
+fn part1(input: &str) -> usize {
+    let orbit_map = gen(input);
+    return orbit_map.keys().fold(0, | acc, object| acc + walk(&orbit_map, object).len());
+}
+
+#[aoc(day6, part2)]
+fn part2(input: &str) -> usize {
+    let orbit_map = gen(input);
+    // Find the paths needed to get from YOU and SAN to COM
+    let you = "YOU".to_owned();
+    let san = "SAN".to_owned();
+    let path1 = walk(&orbit_map, &you);
+    let path2 = walk(&orbit_map, &san);
+    
+    // Walk backwards through the paths from COM -> [YOU|SAN]
+    // Until we find the divergence point, we can then add up the number of
+    // remaining nodes
+    let mut shared_path_length = 0;
+    for (n1, n2) in path1.iter().rev().zip(path2.iter().rev()) {
+        if n1 != n2 {
+            // Have diverged!
+            break;
+        }
+        shared_path_length += 1;
+    }
+    // Orbital transfers from YOU -> SAN is len of the two paths - 2x shared_path_length
+    return path1.len() + path2.len() - (2 * shared_path_length);
+}
--- 2019/src/solutions/mod.rs
+++ 2019/src/solutions/mod.rs
@@ -3,3 +3,4 @@ pub mod day02_1202_program_alarm;
 pub mod day03_crossed_wires;
 pub mod day04_secure_container;
 pub mod day05_sunny_with_a_chance_of_rain;
+pub mod day06_universal_orbit_map;

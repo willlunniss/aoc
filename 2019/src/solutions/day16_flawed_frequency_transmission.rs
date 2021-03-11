47a1a3162a0f28eb6045f4b81bfe5d6f8ce6c7f2
Will Lunniss <willlunniss@gmail.com>
2021-03-11 19:44:40 +0000

    Solution to 2019 day 16 part 2

--- 2019/src/solutions/day16_flawed_frequency_transmission.rs
+++ 2019/src/solutions/day16_flawed_frequency_transmission.rs
@@ -1,12 +1,12 @@
 use itertools::Itertools;
 
 struct PatternGenerator {
-    base_pattern: Vec<i16>,
+    base_pattern: Vec<isize>,
     element: usize,
 }
 
 impl PatternGenerator {
-    fn new(base_pattern: &Vec<i16>, element: usize) -> PatternGenerator {
+    fn new(base_pattern: &Vec<isize>, element: usize) -> PatternGenerator {
         return PatternGenerator {
             base_pattern: base_pattern.clone(),
             element: element,
@@ -15,10 +15,10 @@ impl PatternGenerator {
 }
 
 impl<'a> IntoIterator for &'a PatternGenerator {
-    type Item = i16;
+    type Item = isize;
     type IntoIter = PatternGeneratorIterator<'a>;
 
-    fn into_iter(self)  -> Self::IntoIter {
+    fn into_iter(self) -> Self::IntoIter {
         PatternGeneratorIterator {
             generator: self,
             index: 0,
@@ -34,9 +34,9 @@ pub struct PatternGeneratorIterator<'a> {
     repeat: usize,
 }
 
-impl <'a> Iterator for PatternGeneratorIterator<'a> {
-    type Item = i16;
-    fn next(&mut self) -> Option<i16> {
+impl<'a> Iterator for PatternGeneratorIterator<'a> {
+    type Item = isize;
+    fn next(&mut self) -> Option<isize> {
         // Create a pattern from the base pattern by repeating each value element number of times
         // Keep emitting the next entry in the pattern indefinitely (on the first pass we skip the first element)
         self.repeat += 1;
@@ -55,12 +55,12 @@ impl <'a> Iterator for PatternGeneratorIterator<'a> {
 }
 
 struct FFT {
-    base_pattern: Vec<i16>,
-    signal: Vec<i16>,
+    base_pattern: Vec<isize>,
+    signal: Vec<isize>,
 }
 
 impl FFT {
-    fn new(base_pattern: &Vec<i16>, signal: &Vec<i16>) -> FFT {
+    fn new(base_pattern: &Vec<isize>, signal: &Vec<isize>) -> FFT {
         FFT {
             base_pattern: base_pattern.clone(),
             signal: signal.clone(),
@@ -69,29 +69,64 @@ impl FFT {
 
     fn calculate(&mut self, phases: usize) {
         let mut output = vec![0; self.signal.len()];
-        for phase in 1..=phases {
-            println!("Phase {}", phase);
+        for _phase in 1..=phases {
             // Run for the requested number of phases
             for digit in 0..self.signal.len() {
                 // Calculate the result for this digit
                 // Multiply each digit by the corresponding pattern value and then add up all the results
-                let result : i16 = self.signal.iter().zip(PatternGenerator::new(&self.base_pattern, digit).into_iter().take(self.signal.len()))
-                    .map(|(sig_digit, p_value)| sig_digit * p_value).sum();
-                // Then convert to absolute and take the last digit (library doesn't support negative numbers)
-                output[digit] = i16::abs(result) % 10;
+                let result: isize = self
+                    .signal
+                    .iter()
+                    .zip(
+                        PatternGenerator::new(&self.base_pattern, digit)
+                            .into_iter()
+                            .take(self.signal.len()),
+                    )
+                    .map(|(sig_digit, p_value)| sig_digit * p_value)
+                    .sum();
+                // Then convert to absolute and take the last digit
+                output[digit] = isize::abs(result) % 10;
             }
             std::mem::swap(&mut self.signal, &mut output);
         }
     }
 }
 
+/// Uses a trick that the value calculated by the FFT (for the second half only) is just the
+/// sum of it and previous (backwards) numbers mod 10
+///
+/// e.g. 12345678 -> ????6158
+/// 8 + 7 + 6 + 5 % 10 = 6
+/// 8 + 7 + 6 % 10 = 1
+/// 8 + 7 % 10 = 5
+/// 8 % 10 = 8
+fn backwards_sum_2nd_half(input: &Vec<isize>, phases: usize) -> Vec<isize> {
+    let mut input = input.clone();
+    let mut output = vec![0; input.len()];
+    for _phase in 0..phases {
+        let mut sum = 0;
+        // Start from the end of the input and work backwards (stopping at the halfway point)
+        for index in (input.len() / 2..input.len()).rev() {
+            // Add value for this index to the sum
+            sum += input[index];
+            // Output is sum mod 10
+            output[index] = sum % 10;
+        }
+        std::mem::swap(&mut input, &mut output);
+    }
+    return input;
+}
+
 #[aoc_generator(day16)]
-fn gen(input: &str) -> Vec<i16> {
-    return input.chars().map(|c| c.to_digit(10).unwrap() as i16).collect();
+fn gen(input: &str) -> Vec<isize> {
+    return input
+        .chars()
+        .map(|c| c.to_digit(10).unwrap() as isize)
+        .collect();
 }
 
 #[aoc(day16, part1)]
-fn part1(input: &Vec<i16>) -> String {
+fn part1_fft(input: &Vec<isize>) -> String {
     // Run the input directly through 100 phases and return the first 8 digits
     let mut fft = FFT::new(&[0, 1, 0, -1].to_vec(), input);
     fft.calculate(100);
@@ -99,17 +134,29 @@ fn part1(input: &Vec<i16>) -> String {
 }
 
 #[aoc(day16, part2)]
-fn part2(input: &Vec<i16>) -> String {
-    // FIXME: Need to find a different way to do this as can't scale up to a signal of this size 
-    // Repeat the signal 10000 times
-    let mut signal = Vec::new();
-    for copy in 0..10_000 {
-        signal.extend(input);
-    }
-    let mut fft = FFT::new(&[0, 1, 0, -1].to_vec(), &signal);
-    fft.calculate(100);
+fn part2(input: &Vec<isize>) -> String {
     // Get the message offset as the first 7 digits of the input
     let message_offset = input.iter().take(7).join("").parse::<usize>().unwrap();
+    // Append 10 thousand copies of the input
+    let mut signal = Vec::new();
+    for _copy in 0..10_000 {
+        signal.extend(input);
+    }
+    // Take a shortcut to calculate the output (only creates valid values for the 2nd half of the output)
+    // Can only make use of this because the data we want at the message offset is in the 2nd half
+    let output = backwards_sum_2nd_half(&signal, 100);
     // Return the 8 digits at the message offset
-    return fft.signal.iter().skip(message_offset).take(8).join("");
+    return output.iter().skip(message_offset).take(8).join("");
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_backwards_sum() {
+        let signal = [1, 2, 3, 4, 5, 6, 7, 8].to_vec();
+        let output = backwards_sum_2nd_half(&signal, 4);
+        assert_eq!(output[4..8], [9, 4, 9, 8])
+    }
 }

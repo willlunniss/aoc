6e1ed72659e34cf74360af91152006f288e2d031
Will Lunniss <willlunniss@gmail.com>
2021-04-02 15:47:18 +0100

    Solution for 2019 day 22 part 2

--- 2019/src/solutions/day22_slam_shuffle.rs
+++ 2019/src/solutions/day22_slam_shuffle.rs
@@ -1,3 +1,4 @@
+use modinverse::modinverse;
 use std::convert::Infallible;
 use std::str::FromStr;
 
@@ -39,6 +40,53 @@ impl Technique {
             Self::DealWithIncrement(increment) => (index * increment) % size,
         }
     }
+
+    /// Transforms the technique into a linear congruential function of form f(x) = ax + b mod m
+    /// Returns (a, b)
+    ///
+    /// Based on <https://codeforces.com/blog/entry/72593>
+    fn lcf(&self) -> (i128, i128) {
+        match self {
+            Self::DealIntoNewStack => (-1, -1),
+            Self::Cut(cards) => (1, -cards as i128),
+            Self::DealWithIncrement(increment) => (*increment as i128, 0),
+        }
+    }
+}
+
+/// Composes the supplied techniques as lcfs into a single lcf given the supplied deck size
+fn compose_techniques_lcfs(techniques: &[Technique], deck_size: i128) -> (i128, i128) {
+    techniques
+        .iter()
+        .fold((1, 0), |acc, x| compose_lcfs(acc, x.lcf(), deck_size))
+}
+
+/// Composes two lcf given deck size m
+///
+/// Turning f(x)=ax+b  mod m  and g(x)=cx+d  mod m into
+/// (a,b) ;(c,d)=(ac mod m,bc+d  mod m)
+/// Based on <https://codeforces.com/blog/entry/72593>
+fn compose_lcfs(f: (i128, i128), g: (i128, i128), m: i128) -> (i128, i128) {
+    let (a, b) = f;
+    let (c, d) = g;
+    ((a * c).rem_euclid(m), (b * c + d).rem_euclid(m))
+}
+
+/// Composes a lcf k times based on deck size m using exponentiation by squaring
+///
+/// Based on <https://codeforces.com/blog/entry/72593>
+fn pow_compose(f: (i128, i128), k: i128, m: i128) -> (i128, i128) {
+    let mut g = (1, 0);
+    let mut k = k;
+    let mut f = f;
+    while k > 0 {
+        if k % 2 != 0 {
+            g = compose_lcfs(g, f, m);
+        }
+        k /= 2;
+        f = compose_lcfs(f, f, m);
+    }
+    g
 }
 
 /// Shuffles the deck of cards using the techniques
@@ -69,6 +117,7 @@ fn gen(input: &str) -> Vec<Technique> {
     input.lines().map(|line| line.parse().unwrap()).collect()
 }
 
+/// Simple version of part 1 that shuffles the whole deck
 #[aoc(day22, part1, full_deck)]
 fn part1_full_deck(input: &[Technique]) -> usize {
     // Build a standard deck of space cards
@@ -79,15 +128,34 @@ fn part1_full_deck(input: &[Technique]) -> usize {
     shuffled.iter().position(|&card| card == 2019).unwrap()
 }
 
+/// Optimised version of part 1 that shuffles a single card
 #[aoc(day22, part1, single_card)]
 fn part1_single_card(input: &[Technique]) -> usize {
     // Shuffle just card 2019 and see where it ends up
     shuffle_card(2019, 10_007, input)
 }
 
+/// Version of part 1 using modular arithmetic based on <https://codeforces.com/blog/entry/72593>
+#[aoc(day22, part1, modular_arithmetic)]
+fn part1_modular_arithmetic(input: &[Technique]) -> i128 {
+    // Compose the lcfs for all techniques based on our deck size into a single lcf
+    let deck_size = 10_007;
+    let (a, b) = compose_techniques_lcfs(input, deck_size);
+    // Calculate where card 2019 will end up by computing f(x)=ax+b  mod m
+    (a * 2019 + b).rem_euclid(deck_size)
+}
+
+/// Part 2 using modular arithmetic based on <https://codeforces.com/blog/entry/72593>
 #[aoc(day22, part2)]
-fn part2(input: &[Technique]) -> usize {
-    0
+fn part2_modular_arithmetic(input: &[Technique]) -> i128 {
+    // Compose the lcfs for all techniques based on our deck size into a single lcf
+    let deck_size = 119_315_717_514_047;
+    let (a, b) = compose_techniques_lcfs(input, deck_size);
+    // Compose them passes times into a new single lcf
+    let passes = 101_741_582_076_661;
+    let (a, b) = pow_compose((a, b), passes, deck_size);
+    // Calculate what cards ends up at 2020 by calculating the inverse
+    ((2020 - b) * modinverse(a, deck_size).unwrap()).rem_euclid(deck_size)
 }
 
 #[cfg(test)]

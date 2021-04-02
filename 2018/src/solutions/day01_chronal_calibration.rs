b14a50a5bcaa60709e5fb84822f290e2fe82c3e2
Will Lunniss <willlunniss@gmail.com>
2021-04-02 17:57:28 +0100

    Solution for 2018 day 1

---
+++ 2018/src/solutions/day01_chronal_calibration.rs
@@ -0,0 +1,27 @@
+use std::collections::HashSet;
+
+#[aoc_generator(day1)]
+fn gen(input: &str) -> Vec<isize> {
+    input.lines().map(|line| line.parse::<isize>().unwrap()).collect()
+}
+
+#[aoc(day1, part1)]
+fn part1(input: &[isize]) -> isize {
+    // Simply sum up all the changes to get the final frequency
+    input.iter().sum()
+}
+
+#[aoc(day1, part2)]
+fn part2(input: &[isize]) -> isize {
+    let mut frequencies = HashSet::new();
+    let mut frequency = 0;
+    // Loop over the changes indefinitely
+    for change in input.iter().cloned().cycle() {
+        frequency += change;
+        // Store the frequency, if we have already seen it before then return it
+        if !frequencies.insert(frequency) {
+            return frequency;
+        }
+    }
+    0
+}
--- 2018/src/solutions/mod.rs
+++ 2018/src/solutions/mod.rs
@@ -1,1 +1,1 @@
-
+pub mod day01_chronal_calibration;

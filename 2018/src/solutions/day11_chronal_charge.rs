c8281d538dc35f7179f7d154061f1a6c59daf2f0
Will Lunniss <willlunniss@gmail.com>
2021-05-11 17:23:04 +0100

    Optimised solution for 2018 day 11
    Use a pre-computed Summed-area table to speed up calculations

--- 2018/src/solutions/day11_chronal_charge.rs
+++ 2018/src/solutions/day11_chronal_charge.rs
@@ -1,83 +1,74 @@
 use digits_iterator::DigitsExtension;
-use std::collections::HashMap;
+use rayon::iter::{IntoParallelIterator, ParallelIterator};
+use std::convert::TryFrom;
+
+type Power = isize;
+type Pos = (usize, usize);
+type Size = usize;
+const GRID_SIZE: Size = 300;
 
 // Calculates the power level for a given fuel cell
-fn power_level(x: usize, y: usize, serial_number: usize) -> isize {
+fn power_level(x: usize, y: usize, serial_number: usize) -> Power {
     let rack_id = x + 10;
     let hundreds = (((rack_id * y) + serial_number) * rack_id)
         .digits()
         .rev()
         .nth(2)
-        .unwrap_or(0) as isize;
-    hundreds - 5
+        .unwrap_or(0);
+    Power::try_from(hundreds).unwrap() - 5
 }
 
 /// Finds the grid of the specified size which has the maximum total power
 ///
-/// Returns a tuple of power and x, y positions of that grid
-fn maximise_power(
-    levels: &mut HashMap<(usize, usize), isize>,
-    serial_number: usize,
-    size: usize,
-) -> (isize, (usize, usize)) {
-    let mut max = isize::MIN;
-    let mut pos = (0, 0);
-    for y_base in 1..=300 - size {
-        for x_base in 1..=300 - size {
-            // Calculate the power for a grid of size size at x_base, y_base
-            let power = (y_base..y_base + size)
-                .map(|y| {
-                    (x_base..x_base + size)
-                        .map(|x| {
-                            // Get the level for this specific cell, or calculate it if we haven't done so yet
-                            *levels
-                                .entry((x, y))
-                                .or_insert_with(|| power_level(x, y, serial_number))
-                        })
-                        .sum::<isize>()
-                })
-                .sum::<isize>();
-            if power > max {
-                // Found a grid with a higher power
-                max = power;
-                pos = (x_base, y_base);
-            }
-        }
-    }
-    (max, pos)
+/// Returns a tuple of (size, power, pos) of that grid
+fn maximise_power(summed_levels: &[Vec<Power>], size: Size) -> Option<(Size, Power, Pos)> {
+    // For all (x, y) positions that could fit a size x size sub grid calculate the total power
+    (1..=GRID_SIZE - size)
+        .flat_map(|y| (1..=GRID_SIZE - size).map(move |x| (x, y)))
+        .map(|(x, y)| {
+            // Calculate the power in a grid at (x, y) of size x size using the pre-computed
+            // summed levels using Summed-area table technique
+            let power = summed_levels[y - 1 + size][x - 1 + size] + summed_levels[y - 1][x - 1]
+                - summed_levels[y - 1 + size][x - 1]
+                - summed_levels[y - 1][x - 1 + size];
+            (size, power, (x, y))
+        })
+        .max_by_key(|(_size, power, _pos)| *power)
 }
 
 #[aoc_generator(day11)]
-fn gen(input: &str) -> usize {
-    input.parse().unwrap()
+fn gen(input: &str) -> Vec<Vec<Power>> {
+    let serial_number = input.parse().unwrap();
+    // Calculate the summed levels (for use by Summed-area table technique)
+    let mut summed_levels = vec![vec![0; GRID_SIZE + 1]; GRID_SIZE + 1];
+    for y in 1..=GRID_SIZE {
+        for x in 1..=GRID_SIZE {
+            // Value at cell is equal to it's power level plus total of all cells above/left of it
+            summed_levels[y][x] = power_level(x, y, serial_number)
+                + summed_levels[y - 1][x]
+                + summed_levels[y][x - 1]
+                - summed_levels[y - 1][x - 1];
+        }
+    }
+    summed_levels
 }
 
 #[aoc(day11, part1)]
-fn part1(input: &usize) -> String {
-    let mut levels: HashMap<(usize, usize), isize> = HashMap::new();
+fn part1(input: &[Vec<Power>]) -> String {
     // Simply find the grid of maximum power for size of 3
-    let (_, pos) = maximise_power(&mut levels, *input, 3);
+    let (_, _, pos) = maximise_power(input, 3).unwrap();
     format!("{},{}", pos.0, pos.1)
 }
 
 #[aoc(day11, part2)]
-fn part2(input: &usize) -> String {
-    let mut levels: HashMap<(usize, usize), isize> = HashMap::new();
-    let mut best = (0, 1, (1, 1));
+fn part2(input: &[Vec<Power>]) -> String {
     // Find the grid of size between 1 and 300 that has maximum power
-    // FIXME: This is too slow, must be a better way
-    for size in 1..=300 {
-        let (power, pos) = maximise_power(&mut levels, *input, size);
-        if power > best.0 {
-            // New best
-            best = (power, size, pos);
-        } else if power < (best.0 / 2) {
-            // Now less that half our best, including any more cells will just make it worse
-            // so no point to continue searching
-            break;
-        }
-    }
-    format!("{},{},{}", best.2 .0, best.2 .1, best.1)
+    let (size, _power, pos) = (1..=GRID_SIZE)
+        .into_par_iter()
+        .filter_map(|size| maximise_power(input, size))
+        .max_by_key(|(_size, power, _pos)| *power)
+        .unwrap();
+    format!("{},{},{}", pos.0, pos.1, size)
 }
 
 #[cfg(test)]

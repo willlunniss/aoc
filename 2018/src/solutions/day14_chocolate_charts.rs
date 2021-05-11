5e394508685240aefb585b584b5dd94debc9393e
Will Lunniss <willlunniss@gmail.com>
2021-05-11 17:24:18 +0100

    Optimised solution for 2018 day 14

--- 2018/src/solutions/day14_chocolate_charts.rs
+++ 2018/src/solutions/day14_chocolate_charts.rs
@@ -8,22 +8,15 @@ fn gen(input: &str) -> usize {
 
 #[aoc(day14, part1)]
 fn part1(input: &usize) -> String {
-    let elves = 2;
     let mut recipes: Vec<u8> = [3, 7].to_vec();
-    let mut current_indexes: Vec<usize> = (0..elves).collect();
+    let mut current_indexes = [0, 1];
     while recipes.len() < input + 10 {
         // Create new recipes based on the current ones
-        let current: Vec<u8> = current_indexes
-            .iter()
-            .map(|index| recipes[*index])
-            .collect();
+        let current = [recipes[current_indexes[0]], recipes[current_indexes[1]]];
         recipes.extend(current.iter().sum::<u8>().digits());
         // Advance the index by 1 + value of current recipes (wrapping around if needed)
-        current_indexes = current_indexes
-            .iter()
-            .zip(current)
-            .map(|(index, recipe)| (index + 1 + recipe as usize) % recipes.len())
-            .collect();
+        current_indexes[0] = (current_indexes[0] + 1 + current[0] as usize) % recipes.len();
+        current_indexes[1] = (current_indexes[1] + 1 + current[1] as usize) % recipes.len();
     }
     // Return the scores of the 10 recipes after our input
     recipes
@@ -35,18 +28,14 @@ fn part1(input: &usize) -> String {
 
 #[aoc(day14, part2)]
 fn part2(input: &usize) -> usize {
-    let elves = 2;
     let mut recipes: Vec<u8> = [3, 7].to_vec();
-    let mut current_indexes: Vec<usize> = (0..elves).collect();
+    let mut current_indexes = [0, 1];
     // Store what we are searching for in reverse
     let search: Vec<u8> = input.digits().rev().collect();
     let search_len = search.len();
     loop {
         // Create new recipes based on the current ones
-        let current: Vec<u8> = current_indexes
-            .iter()
-            .map(|index| recipes[*index])
-            .collect();
+        let current = [recipes[current_indexes[0]], recipes[current_indexes[1]]];
         for new in current.iter().sum::<u8>().digits() {
             // Add each new recipe individually
             recipes.push(new);
@@ -66,10 +55,7 @@ fn part2(input: &usize) -> usize {
             }
         }
         // Advance the index by 1 + value of current recipes (wrapping round if needed)
-        current_indexes = current_indexes
-            .iter()
-            .zip(current)
-            .map(|(index, recipe)| (index + 1 + recipe as usize) % recipes.len())
-            .collect();
+        current_indexes[0] = (current_indexes[0] + 1 + current[0] as usize) % recipes.len();
+        current_indexes[1] = (current_indexes[1] + 1 + current[1] as usize) % recipes.len();
     }
 }

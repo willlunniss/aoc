af37f3e9942d5468a041d9827221ca8d870b1490
Will Lunniss <willlunniss@gmail.com>
2021-12-03 14:59:18 +0800

    Tidy up solution for 2021 day 3

--- 2021/src/solutions/day03_binary_diagnostic.rs
+++ 2021/src/solutions/day03_binary_diagnostic.rs
@@ -1,7 +1,10 @@
 #[aoc(day3, part1)]
 fn part1(input: &str) -> usize {
-    let mut counts = vec![0; 12];
+    // Init an array with a 0 for each bit
+    let bits = input.lines().next().unwrap().len();
+    let mut counts = vec![0; bits];
     for number in input.lines() {
+        // For each bit, find out if 1 or 0 is most common
         for (index, c) in number.chars().enumerate() {
             if c == '1' {
                 counts[index] += 1;
@@ -10,56 +13,54 @@ fn part1(input: &str) -> usize {
             }
         }
     }
-    let gamma_rate = counts.iter().rev().enumerate().map(|(i, x)| if *x > 0 { 1 << i } else { 0 } ).sum::<usize>();
-    let epsilon_rate = gamma_rate ^ ((1 << 12) - 1);
+    // For each bit (LSB -> MSB), gamma rate using the most common bit from all numbers
+    let gamma_rate = counts
+        .iter()
+        .rev()
+        .enumerate()
+        .map(|(i, x)| if *x > 0 { 1 << i } else { 0 })
+        .sum::<usize>();
+    // Epsilon rate uses the least common (xor with all bits set)
+    let epsilon_rate = gamma_rate ^ ((1 << bits) - 1);
+    // Result is the product of the two
     gamma_rate * epsilon_rate
+}
+
+fn filter(input: &str, keep_set: bool) -> usize {
+    // Check how many bits each number has
+    let bits = input.lines().next().unwrap().len();
+    // Interoperate each line as a binary number
+    let mut values = input
+        .lines()
+        .map(|number| usize::from_str_radix(number, 2).unwrap())
+        .collect::<Vec<_>>();
+    for index in 1.. {
+        let bit = bits - index;
+        // Count how many bits at this index are set across all numbers
+        let count = values
+            .iter()
+            .map(|number| if number & (1 << bit) == 0 { -1 } else { 1 })
+            .sum::<isize>();
+        // Depending on mode, work out what we want to keep
+        let keep = !((count >= 0) ^ keep_set);
+        // Keep all values that have the right state for the bit at this index
+        values = values
+            .iter()
+            .filter(|&number| (number & (1 << bit) != 0) == keep)
+            .copied()
+            .collect();
+        if values.len() == 1 {
+            // Until we only have one value left
+            break;
+        }
+    }
+    // Interpret as a binary number and return decimal value
+    *values.first().unwrap()
 }
 
 #[aoc(day3, part2)]
 fn part2(input: &str) -> usize {
-    let length = 12;
-    let mut oxygen = input.lines().collect::<Vec<_>>();
-    let mut o2 = oxygen.clone();
-    for index in 0..length {
-        if oxygen.len() > 1 {
-            let mut keep = Vec::new();
-            let mut count = 0;
-            for number in &oxygen {
-                if number.chars().nth(index).unwrap() == '1' {
-                    count += 1;
-                } else {
-                    count -= 1;
-                }
-            }
-            let test = if count >= 0 { '1' } else { '0' };
-            for number in &oxygen {
-                if number.chars().nth(index).unwrap() == test {
-                    keep.push(*number);
-                }
-            }
-            oxygen = keep;
-        }
-        
-        if o2.len() > 1 {
-            let mut keep = Vec::new();
-            let mut count = 0;
-            for number in &o2 {
-                if number.chars().nth(index).unwrap() == '1' {
-                    count += 1;
-                } else {
-                    count -= 1;
-                }
-            }
-            let test = if count >= 0 { '0' } else { '1' };
-            for number in &o2 {
-                if number.chars().nth(index).unwrap() == test {
-                    keep.push(*number);
-                }
-            }
-            o2 = keep;
-        }
-    }
-    usize::from_str_radix(oxygen.first().unwrap(), 2).unwrap() * usize::from_str_radix(o2.first().unwrap(), 2).unwrap()
+    filter(input, true) * filter(input, false)
 }
 
 #[cfg(test)]
@@ -84,6 +85,7 @@ mod tests {
 
     #[test]
     fn test_part1_example() {
+        assert_eq!(part1(EXAMPLE_INPUT), 198);
     }
 
     #[test]

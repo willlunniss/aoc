bf802fd142498c712d1448038edf7993213340f1
Will Lunniss <willlunniss@gmail.com>
2021-12-07 09:15:32 +0800

    Optimised solution for 2021 day 6

--- 2021/src/solutions/day06_lanternfish.rs
+++ 2021/src/solutions/day06_lanternfish.rs
@@ -13,7 +13,7 @@ fn part1_simulate(input: &Vec<usize>) -> usize {
     // Naive approach - simulate what happens one day at a time
     // Only feasible for small numbers of days
     let mut fishes = input.clone();
-    for _day in 0..80 {
+    for _day in 1..=80 {
         let mut next = Vec::new();
         for fish in fishes {
             // For each fish, if their timer is at 0 create a new one at 8 and add them at 6
@@ -30,17 +30,7 @@ fn part1_simulate(input: &Vec<usize>) -> usize {
     fishes.len()
 }
 
-#[aoc(day6, part1, compute)]
-fn part1_compute(input: &[usize]) -> usize {
-    input.iter().map(|&timer| calculate(timer, 80)).sum()
-}
-
-#[aoc(day6, part2)]
-fn part2(input: &[usize]) -> usize {
-    input.iter().map(|&timer| calculate(timer, 256)).sum()
-}
-
-/// Returns how many fish there will be after the supplied days given current timer
+/// Recursively calculates how many fish there will be after the supplied days given current timer
 #[cached]
 fn calculate(timer: usize, days: usize) -> usize {
     if days <= timer {
@@ -56,6 +46,46 @@ fn calculate(timer: usize, days: usize) -> usize {
 
     // Total is 1 + for each completed cycle, how many fish given an initial timer of 8 based taking into account when it will be spawned
     1 + (0..completed_cycles).map(|cycle| calculate(8, days_after_first_cycle - (cycle * 7))).sum::<usize>()
+}
+
+/// Optimised count of how many fish there will be after a certain number of days
+fn quick_count(fishes: &[usize], days: usize) -> usize {
+    // Init a circular buffer with the number of fish with each timer value
+    let mut fish_with_timer = vec![0; 9];
+    for &initial_timer in fishes {
+        fish_with_timer[initial_timer] += 1;
+    }
+
+    let mut pos = 0;
+    for _day in 1..=days {
+        // For each day, get the number of fish that have a timer of 0 and remove them
+        let completed_cycle = fish_with_timer[pos];
+        fish_with_timer[pos] = 0;
+
+        // Then add the new fish in what will be the +6 and +8 slots
+        fish_with_timer[(pos + 1 + 6) % 9] += completed_cycle;
+        fish_with_timer[(pos + 1 + 8) % 9] += completed_cycle;
+
+        // Advance the position forward by 1
+        pos = (pos + 1) % 9;
+    }
+    // Result is the number of fish at the end
+    fish_with_timer.iter().sum()
+}
+
+#[aoc(day6, part1, compute)]
+fn part1_compute(input: &[usize]) -> usize {
+    input.iter().map(|&timer| calculate(timer, 80)).sum()
+}
+
+#[aoc(day6, part2, compute)]
+fn part2_compute(input: &[usize]) -> usize {
+    input.iter().map(|&timer| calculate(timer, 256)).sum()
+}
+
+#[aoc(day6, part2, quick_count)]
+fn part2_quick_count(input: &[usize]) -> usize {
+    quick_count(input, 256)
 }
 
 #[cfg(test)]
@@ -76,7 +106,17 @@ mod tests {
 
     #[test]
     fn test_part2_example() {
-        assert_eq!(part2(&gen(EXAMPLE_INPUT)), 26_984_457_539);
+        assert_eq!(part2_compute(&gen(EXAMPLE_INPUT)), 26_984_457_539);
+    }
+
+    #[test]
+    fn test_part2_example_quick_count() {
+        assert_eq!(part2_quick_count(&gen(EXAMPLE_INPUT)), 26_984_457_539);
+    }
+
+    #[test]
+    fn test_quick_count() {
+        assert_eq!(quick_count(&gen(EXAMPLE_INPUT), 18), 26);
     }
 
     #[test]

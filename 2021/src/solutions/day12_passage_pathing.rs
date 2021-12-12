b7dce72351b0b881072810335409159bceb71779
Will Lunniss <willlunniss@gmail.com>
2021-12-12 14:30:55 +0800

    Optimise solution for 2021 day 12

--- 2021/src/solutions/day12_passage_pathing.rs
+++ 2021/src/solutions/day12_passage_pathing.rs
@@ -1,7 +1,7 @@
 use itertools::Itertools;
 use std::collections::HashMap;
-use std::collections::VecDeque;
 use std::collections::HashSet;
+use std::collections::VecDeque;
 
 #[derive(Debug, Clone)]
 struct Cave<'a> {
@@ -9,15 +9,21 @@ struct Cave<'a> {
 }
 
 fn gen(input: &str) -> HashMap<&str, Cave> {
-    let mut caves : HashMap<&str, Cave> = HashMap::new();
+    let mut caves: HashMap<&str, Cave> = HashMap::new();
     for (from, to) in input
         .lines()
         .map(|line| line.split('-').collect_tuple().unwrap())
     {
-        caves.entry(from).and_modify(|x| x.to.push(to)).or_insert(Cave{to: vec![to; 1]});
-        caves.entry(to).and_modify(|x| x.to.push(from)).or_insert(Cave{to: vec![from; 1]});
+        // For each each connected cave record that we can go between them
+        caves
+            .entry(from)
+            .and_modify(|x| x.to.push(to))
+            .or_insert(Cave { to: vec![to; 1] });
+        caves
+            .entry(to)
+            .and_modify(|x| x.to.push(from))
+            .or_insert(Cave { to: vec![from; 1] });
     }
-
     caves
 }
 
@@ -25,86 +31,58 @@ fn is_small_cave(name: &str) -> bool {
     name.chars().all(char::is_lowercase)
 }
 
-#[aoc(day12, part1)]
-fn part1(input: &str) -> usize {
-    let caves = gen(input);
+/// Counts the number of distinct paths from `start` --> `end`
+///
+/// For each path, big caves can be visited multiple times and small caves can be visited once
+/// Exception being is that if `extra_visit` is true, then a single small cave can
+/// be visited twice
+fn count_paths(caves: &HashMap<&str, Cave>, extra_visit: bool) -> usize {
     let mut queue = VecDeque::new();
-    let mut paths = Vec::new();
-    queue.push_back(("start", vec!["start"; 1], HashSet::new()));
-    while let Some((name, route, visited_small)) = queue.pop_front() {
-        // Get the cave
+    let mut paths = 0;
+    queue.push_back(("start", HashSet::new(), !extra_visit));
+    while let Some((name, visited_small, used_extra_visit)) = queue.pop_front() {
+        // Consider all the places we could move to from this cave
         let cave = caves.get(name).unwrap();
         for next in &cave.to {
-            let mut route = route.clone();
             if next == &"start" {
                 // don't go back to the start
                 continue;
-            } 
+            }
             if next == &"end" {
-                // Found a complete route
-                route.push(next);
-                paths.push(route);
+                // Found a complete path
+                paths += 1;
                 continue;
-            } 
-            // Haven't been here yet, for this route
+            }
+            // Haven't been here yet, for this path
             if !is_small_cave(next) {
-                // Move to it
-                //route.push(next);
-                queue.push_back((next, route, visited_small.clone()));
+                // Can visit big caves multiple times, move into it
+                queue.push_back((next, visited_small.clone(), used_extra_visit));
             } else if !visited_small.contains(next) {
-                // Move to it
-                //route.push(next);
-                let mut visited_small_next = visited_small.clone();
-                visited_small_next.insert(next);
-                queue.push_back((next, route, visited_small_next));
+                // Haven't visited this small cave before, move into it recording that
+                // we have visited it
+                let mut visited_small = visited_small.clone();
+                visited_small.insert(next);
+                queue.push_back((next, visited_small, used_extra_visit));
+            } else if !used_extra_visit {
+                // Have visited this small cave before, but haven't used our extra visit yet
+                // Use it up and move into it
+                queue.push_back((next, visited_small.clone(), true));
             }
         }
     }
-    paths.len()
+    paths
+}
+
+#[aoc(day12, part1)]
+fn part1(input: &str) -> usize {
+    // Count all paths, only visiting small caves once per path
+    count_paths(&gen(input), false)
 }
 
 #[aoc(day12, part2)]
 fn part2(input: &str) -> usize {
-    let caves = gen(input);
-    let mut queue = VecDeque::new();
-    let mut paths = Vec::new();
-    queue.push_back(("start", vec!["start"; 1], HashMap::new(), false));
-    while let Some((name, route, visited_small, used_double_visit)) = queue.pop_front() {
-        // Get the cave
-        let cave = caves.get(name).unwrap();
-        for next in &cave.to {
-            let mut route = route.clone();
-            if next == &"start" {
-                // don't go back to the start
-                continue;
-            } 
-            if next == &"end" {
-                // Found a complete route
-                route.push(next);
-                paths.push(route);
-                continue;
-            } 
-            // Haven't been here yet, for this route
-            if !is_small_cave(next) {
-                // Move to it
-                route.push(next);
-                queue.push_back((next, route, visited_small.clone(), used_double_visit));
-            } else {
-                let mut visited_small_next = visited_small.clone();
-                let visits = visited_small_next.entry(next).and_modify(|x| { *x += 1 }).or_insert(1);
-                if *visits < 2 || (!used_double_visit && *visits < 3) {
-                    // Move to it
-                    route.push(next);
-                    let mut have_now_used_it = used_double_visit;
-                    if *visits > 1 {
-                        have_now_used_it = true;
-                    }
-                    queue.push_back((next, route, visited_small_next, have_now_used_it));
-                }
-            }
-        }
-    }
-    paths.len()
+    // Count all paths, allowing a single extra visit to a small cave per path
+    count_paths(&gen(input), true)
 }
 
 #[cfg(test)]

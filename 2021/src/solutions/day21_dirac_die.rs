3a62aef2c92698e15061b0ba2cea03dfc906cb0f
Will Lunniss <willlunniss@gmail.com>
2021-12-21 18:53:07 +0800

    Initial solution for 2021 day 21 part 2

--- 2021/src/solutions/day21_dirac_die.rs
+++ 2021/src/solutions/day21_dirac_die.rs
@@ -1,3 +1,4 @@
+use cached::proc_macro::cached;
 use itertools::Itertools;
 
 #[aoc_generator(day21)]
@@ -25,7 +26,7 @@ impl Player {
         self.score >= requires
     }
 
-    fn play(&mut self, spaces: u32, requires: u32) -> bool {
+    fn play_turn(&mut self, spaces: u32, requires: u32) -> bool {
         self.space += spaces;
         while self.space > 10 {
             self.space -= 10;
@@ -63,23 +64,81 @@ impl DeterministicDice {
     }
 }
 
+lazy_static! {
+    /// Possible outcomes of rolling dirac die 3 times
+    static ref DIRAC_ROLL_3_SUMS: Vec<u8> = [1, 2, 3, 1, 2, 3, 1, 2, 3]
+        .iter()
+        .combinations(3)
+        .unique()
+        .map(|x| { x.iter().copied().sum() })
+        .collect();
+}
+
+#[cached]
+fn dirac_winner(p1_pos: u8, p1_score: u8, p2_pos: u8, p2_score: u8) -> (u128, u128) {
+    let mut wins = (0, 0);
+    // For each possible outcome of player 1 rolling
+    for p1_roll in DIRAC_ROLL_3_SUMS.iter() {
+        // For each possible outcome of player 2 rolling
+        for p2_roll in DIRAC_ROLL_3_SUMS.iter() {
+            // Create new parallel players for each outcome by purposefully re-defining passed in values
+            // Move players based on their rolls
+            let mut p1_pos = p1_pos + p1_roll;
+            if p1_pos > 10 {
+                p1_pos -= 10;
+            }
+            let mut p2_pos = p2_pos + p2_roll;
+            if p2_pos > 10 {
+                p2_pos -= 10;
+            }
+
+            // Calculate scores
+            let p1_score = p1_score + p1_pos;
+            let p2_score = p2_score + p2_pos;
+
+            // See if someone won
+            if p1_score >= 21 {
+                wins.0 += 1;
+                // This game and all parallel games for this roll of player 1 end
+                break;
+            } else if p2_score >= 21 {
+                wins.1 += 1;
+            } else {
+                // No one won - game continues
+                let new_wins = dirac_winner(p1_pos, p1_score, p2_pos, p2_score);
+                wins.0 += new_wins.0;
+                wins.1 += new_wins.1;
+            }
+        }
+    }
+    wins
+}
+
 #[aoc(day21, part1)]
 fn part1(input: &[Player]) -> u32 {
     let (mut player1, mut player2) = (input[0].clone(), input[1].clone());
     let mut die = DeterministicDice::new();
+    // Play with deterministic die until someone wins
     loop {
-        if player1.play(die.roll3sum(), 1000) {
+        if player1.play_turn(die.roll3sum(), 1000) {
             return player2.score * die.rolls;
         }
-        if player2.play(die.roll3sum(), 1000) {
+        if player2.play_turn(die.roll3sum(), 1000) {
             return player1.score * die.rolls;
         }
     }
 }
 
 #[aoc(day21, part2)]
-fn part2(input: &[Player]) -> u32 {
-    0
+fn part2(input: &[Player]) -> u128 {
+    // Find out who wins most for the given starting position using dirac die
+    let wins = dirac_winner(
+        input[0].space.try_into().unwrap(),
+        0,
+        input[1].space.try_into().unwrap(),
+        0,
+    );
+    u128::max(wins.0, wins.1)
 }
 
 #[cfg(test)]
@@ -94,11 +153,11 @@ mod tests {
 
     #[test]
     fn test_part1_example() {
-        assert_eq!(part1(&gen(EXAMPLE_INPUT)), 739785);
+        assert_eq!(part1(&gen(EXAMPLE_INPUT)), 739_785);
     }
 
     #[test]
     fn test_part2_example() {
-        assert_eq!(part2(&gen(EXAMPLE_INPUT)), 0);
+        assert_eq!(part2(&gen(EXAMPLE_INPUT)), 444_356_092_776_315);
     }
 }

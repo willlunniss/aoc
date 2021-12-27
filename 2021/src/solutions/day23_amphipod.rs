c14a122e614f7db32fd6a79e118c8c4a5814706c
Will Lunniss <willlunniss@gmail.com>
2021-12-27 11:43:33 +0800

    WIP optimise solution for 2021 day 23

--- 2021/src/solutions/day23_amphipod.rs
+++ 2021/src/solutions/day23_amphipod.rs
@@ -1,8 +1,10 @@
 use itertools::Itertools;
+use pathfinding::prelude::dijkstra;
 use std::cmp::Ordering;
 use std::collections::hash_map::Entry;
-use std::collections::{BinaryHeap, HashMap, HashSet, VecDeque};
+use std::collections::{HashMap, HashSet, VecDeque};
 use std::fmt;
+use std::hash::{Hash, Hasher};
 use utils::grid::{Pos, VecGrid};
 
 fn requires_energy(amphipod: char) -> usize {
@@ -31,12 +33,24 @@ fn targets(amphipod: char) -> (Pos, Pos, Pos, Pos) {
     )
 }
 
-fn is_target(amphipod: char, pos: &Pos, spaces: &HashSet<Pos>) -> bool {
+fn is_target(
+    amphipod: char,
+    pos: &Pos,
+    spaces: &HashSet<Pos>,
+    amphipods: &HashMap<Pos, char>,
+) -> bool {
     let (t1, t2, t3, t4) = targets(amphipod);
-    *pos == t1
-        || { *pos == t2 && !spaces.contains(&t1) }
-        || { *pos == t3 && !spaces.contains(&t2) }
-        || { *pos == t4 && !spaces.contains(&t3) }
+    if *pos == t1 {
+        true
+    } else if *pos == t2 {
+        !spaces.contains(&t1) && !amphipods.contains_key(&t1)
+    } else if *pos == t3 {
+        !spaces.contains(&t2) && !amphipods.contains_key(&t2)
+    } else if *pos == t4 {
+        !spaces.contains(&t3) && !amphipods.contains_key(&t3)
+    } else {
+        false
+    }
 }
 
 const fn in_hallway(pos: &Pos) -> bool {
@@ -61,32 +75,26 @@ fn can_move_to(
     let mut queue = VecDeque::new();
     queue.push_back((*start, 0));
     while let Some((pos, moves)) = queue.pop_front() {
-        for candidate in pos
-            .neighbours()
-            .filter(|pos| spaces.contains(pos) && !amphipods.contains_key(pos))
-        {
+        for candidate in pos.neighbours().filter(|pos| spaces.contains(pos)) {
             if let Entry::Vacant(e) = candidates.entry(candidate) {
                 e.insert(moves + 1);
                 queue.push_back((candidate, moves + 1));
             }
         }
     }
-    if let Some((pos, cost)) = candidates
-        .iter()
-        .find(|(pos, _)| is_target(amphipod, pos, spaces))
-    {
-        // If we can move directly to out target then always do that
-        return vec![(*pos, *cost); 1];
-    }
     return candidates
         .iter()
-        .filter(|(pos, _)| can_stop(pos) && (in_hallway(pos) && !in_hallway(start)))
+        .filter(|(pos, _)| {
+            can_stop(pos)
+                && ((in_hallway(pos) && !in_hallway(start))
+                    || is_target(amphipod, pos, spaces, amphipods))
+        })
         .map(|(k, v)| (*k, *v))
         .collect();
 }
 
 fn debug(spaces: &HashSet<Pos>, amphipods: &HashMap<Pos, char>, energy: usize) {
-    let mut grid = VecGrid::new_sized('#', 14, 5);
+    let mut grid = VecGrid::new_sized('#', 14, 7);
     for pos in spaces {
         grid[*pos] = '.';
     }
@@ -97,27 +105,28 @@ fn debug(spaces: &HashSet<Pos>, amphipods: &HashMap<Pos, char>, energy: usize) {
     grid.print();
 }
 
+#[derive(Clone)]
 struct State {
-    energy: usize,
     spaces: HashSet<Pos>,
     amphipods: HashMap<Pos, char>,
 }
 
-impl State {
-    const fn new(spaces: HashSet<Pos>, amphipods: HashMap<Pos, char>, energy: usize) -> Self {
-        Self {
-            energy,
-            spaces,
-            amphipods,
-        }
+impl Hash for State {
+    fn hash<H: Hasher>(&self, state: &mut H) {
+        self.amphipods
+            .iter()
+            .sorted()
+            .collect::<Vec<_>>()
+            .hash(state);
     }
 }
 
 impl PartialEq for State {
     fn eq(&self, other: &Self) -> bool {
-        self.energy == other.energy
-            && self.spaces == other.spaces
-            && self.amphipods == other.amphipods
+        self.amphipods
+            .iter()
+            .sorted()
+            .eq(other.amphipods.iter().sorted())
     }
 }
 
@@ -131,95 +140,85 @@ impl PartialOrd for State {
 
 impl Ord for State {
     fn cmp(&self, other: &Self) -> Ordering {
-        let remaining = other.amphipods.len().cmp(&self.amphipods.len());
-        let energy = other.energy.cmp(&self.energy);
-        if energy == Ordering::Equal {
-            remaining
-        } else {
-            energy
-        }
+        self.amphipods.len().cmp(&other.amphipods.len())
+    }
+}
+
+impl State {
+    const fn new(spaces: HashSet<Pos>, amphipods: HashMap<Pos, char>) -> Self {
+        Self { spaces, amphipods }
+    }
+
+    fn organised(&self) -> bool {
+        self.amphipods.is_empty()
     }
 }
 
 impl fmt::Debug for State {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         f.debug_struct("State")
-            .field("energy", &self.energy)
             .field("remaining", &self.amphipods.len())
             .finish()
     }
 }
 
-fn organise(input: &VecGrid<char>) -> usize {
+fn organise_amphipods(input: &VecGrid<char>) -> usize {
     let spaces = input
         .into_iter()
-        .filter(|(_, &x)| x == '.' || is_amphipod(x))
+        .filter(|(_, &x)| x == '.')
         .map(|(pos, _)| pos)
         .collect::<HashSet<Pos>>();
-    let amphipods = input
+    let mut amphipods = input
         .into_iter()
         .filter(|(_, &x)| is_amphipod(x))
         .map(|(pos, &a)| (pos, a))
         .collect::<HashMap<Pos, char>>();
-
-    // Search for the moves that require the lowest energy to organise
-    let mut results = Vec::new();
-    let mut queue = BinaryHeap::new();
-    queue.push(State::new(spaces.clone(), amphipods.clone(), 0));
-    // Track which states we have exploded and at what energy cost
-    let mut explored: HashMap<Vec<(Pos, char)>, usize> = HashMap::new();
-
-    while !queue.is_empty() {
-        // Get the next state to consider with the lowest energy cost so far (ties broken by least amphipods to move)
-        let state = queue.pop().unwrap();
-
-        // Consider all amphipods
-        for (pos, &amphipod) in &state.amphipods {
-            let candidates = can_move_to(amphipod, pos, &state.spaces, &state.amphipods);
-            if candidates.is_empty() {
-                continue;
-            }
-            // Check where they could go to
-            for (candidate, moves) in candidates {
-                let mut amphipods = state.amphipods.clone();
-                let mut spaces = state.spaces.clone();
-                // Move to the new state
-                let energy = state.energy + (moves * requires_energy(amphipod));
-                amphipods.remove(pos);
-                if is_target(amphipod, &candidate, &spaces) {
-                    // At the target so can stop having to consider this amphipod
-                    spaces.remove(&candidate);
-                } else {
-                    amphipods.insert(candidate, amphipod);
-                }
-                if amphipods.is_empty() {
-                    // Found a valid solution
-                    results.push(energy);
-                } else {
-                    // Check to see if we have considered this state before
-                    let best = explored
-                        .entry(
-                            amphipods
-                                .iter()
-                                .map(|(k, v)| (*k, *v))
-                                .sorted()
-                                .collect::<Vec<_>>(),
-                        )
-                        .or_insert(energy);
-                    if *best >= energy {
-                        queue.push(State::new(spaces, amphipods, energy));
-                    } // else - have seen this state before at a lower energy so no point considering again
-                }
-            }
-        }
+    let in_target = amphipods
+        .iter()
+        .filter(|(pos, &amphipod)| is_target(amphipod, pos, &spaces, &amphipods))
+        .map(|(pos, _)| *pos)
+        .collect::<Vec<_>>();
+    for pos in in_target {
+        amphipods.remove(&pos);
     }
-
-    *results.iter().min().unwrap()
+    let initial = State::new(spaces.clone(), amphipods.clone());
+    dijkstra(
+        &initial,
+        |state| {
+            let mut next = Vec::new();
+            // Consider all amphipods
+            for (pos, amphipod) in &state.amphipods {
+                let candidates = can_move_to(*amphipod, pos, &state.spaces, &state.amphipods);
+                if candidates.is_empty() {
+                    continue;
+                }
+                // Check where they could go to
+                for (candidate, moves) in candidates {
+                    let mut amphipods = state.amphipods.clone();
+                    let mut spaces = state.spaces.clone();
+                    // Move to the new state
+                    amphipods.remove(pos);
+                    if !is_target(*amphipod, &candidate, &spaces, &state.amphipods) {
+                        amphipods.insert(candidate, *amphipod);
+                    }
+                    // Free up the space where we were and remove where we have gone to
+                    spaces.insert(*pos);
+                    spaces.remove(&candidate);
+                    let energy = moves * requires_energy(*amphipod);
+                    next.push((State::new(spaces, amphipods), energy));
+                }
+            }
+            next
+        },
+        State::organised,
+    )
+    .unwrap()
+    .1
 }
 
 #[aoc(day23, part1)]
 fn part1(input: &str) -> usize {
-    organise(&input.parse().unwrap())
+    organise_amphipods(&input.parse().unwrap())
 }
 
 static P2_INSERT: [&str; 2] = ["  #D#C#B#A#", "  #D#B#A#C#"];
@@ -228,7 +227,7 @@ static P2_INSERT: [&str; 2] = ["  #D#C#B#A#", "  #D#B#A#C#"];
 fn part2(input: &str) -> usize {
     let mut lines = input.lines().collect::<Vec<_>>();
     lines.splice(3..3, P2_INSERT);
-    organise(&lines.into())
+    organise_amphipods(&lines.into())
 }
 
 #[cfg(test)]

579729c6242f9541de8dc61e48f0c7e2cabc8e8d
Will Lunniss <willlunniss@gmail.com>
2022-02-20 13:30:03 +0800

    Tidy up solution for 2016 day 20

--- 2016/src/solutions/day20_firewall_rules.rs
+++ 2016/src/solutions/day20_firewall_rules.rs
@@ -1,47 +1,37 @@
 use itertools::Itertools;
 
-#[aoc_generator(day20)]
-fn gen(input: &str) -> Vec<(usize, usize)> {
+/// Returns the allowed IPs based on the supplied ranges
+fn allowed(input: &str) -> impl Iterator<Item = usize> + '_ {
+    let mut current = 0;
     input
         .lines()
         .map(|line| {
+            // Read in the ranges
             line.split('-')
                 .map(|x| x.parse().unwrap())
                 .collect_tuple::<(usize, usize)>()
                 .unwrap()
         })
-        .sorted_by(|a, b| b.0.cmp(&a.0)) // Pre-sort the ranges in reverse order by min value
-        .collect()
-}
-
-/// Returns the list of IPs allowed by the pre-sorted ranges
-fn allowed(ranges: &[(usize, usize)]) -> Vec<usize> {
-    let mut ranges = ranges.to_vec();
-    let mut allowed = Vec::new();
-    let mut current = 0;
-    while let Some((min, max)) = ranges.pop() {
-        if current < min {
-            // The next minimum is below our current best value
-            // All between best and min are allowed
-            allowed.extend(current..min);
-            current = min;
-        }
-        if max >= current {
-            // Advance the current value to be after the end of the range
-            current = max + 1;
-        }
-    }
-    allowed
+        .sorted_by(|a, b| a.0.cmp(&b.0)) // Sort from lowest to highest
+        .flat_map(move |(min, max)| {
+            // Allow all IPs between current and next min. If current >= min then this range will be empty
+            let allowed = current..min;
+            if max >= current {
+                // Advance the current value to be after the end of the range
+                current = max + 1;
+            }
+            allowed
+        })
 }
 
 #[aoc(day20, part1)]
-fn part1(input: &[(usize, usize)]) -> usize {
-    *allowed(input).first().unwrap()
+fn part1(input: &str) -> usize {
+    allowed(input).next().unwrap()
 }
 
 #[aoc(day20, part2)]
-fn part2(input: &[(usize, usize)]) -> usize {
-    allowed(input).len()
+fn part2(input: &str) -> usize {
+    allowed(input).count()
 }
 
 #[cfg(test)]
@@ -57,6 +47,6 @@ mod tests {
 
     #[test]
     fn test_part1_example() {
-        assert_eq!(part1(&gen(EXAMPLE_INPUT)), 3);
+        assert_eq!(part1(EXAMPLE_INPUT), 3);
     }
 }

27701025b7773c824dfcbaa633de869ad67e5d46
Will Lunniss <willlunniss@gmail.com>
2022-04-16 11:26:16 +0800

    Optimised solution for 2016 day 21

--- 2016/src/solutions/day21_scrambled_letters_and_hash.rs
+++ 2016/src/solutions/day21_scrambled_letters_and_hash.rs
@@ -1,4 +1,3 @@
-use itertools::Itertools;
 use std::collections::VecDeque;
 use std::str::FromStr;
 
@@ -56,6 +55,7 @@ fn gen(input: &str) -> Vec<Operation> {
     input.lines().map(|line| line.parse().unwrap()).collect()
 }
 
+/// Scrambles a phrase using the supplied operations
 fn scramble(phrase: &str, ops: &[Operation]) -> String {
     let mut chars = phrase.chars().collect::<VecDeque<_>>();
     for op in ops {
@@ -64,6 +64,7 @@ fn scramble(phrase: &str, ops: &[Operation]) -> String {
                 chars.swap(*x, *y);
             }
             Operation::SwapLetter(a, b) => {
+                // Find the position of the letters and then swap
                 let (x, y) = (
                     chars.iter().position(|l| l == a).unwrap(),
                     chars.iter().position(|l| l == b).unwrap(),
@@ -73,6 +74,7 @@ fn scramble(phrase: &str, ops: &[Operation]) -> String {
             Operation::RotateLeft(steps) => chars.rotate_left(*steps),
             Operation::RotateRight(steps) => chars.rotate_right(*steps),
             Operation::RotateBasedOnPositionOfLetter(a) => {
+                // Number of steps to rotate based on the position of the letter
                 let mut steps = chars.iter().position(|l| l == a).unwrap();
                 if steps >= 4 {
                     steps += 1;
@@ -80,6 +82,7 @@ fn scramble(phrase: &str, ops: &[Operation]) -> String {
                 chars.rotate_right((1 + steps) % chars.len());
             }
             Operation::ReversePositions(x, y) => {
+                // Reverse chars between x..=y
                 for (i, c) in (*x..=*y)
                     .rev()
                     .map(|i| chars[i])
@@ -99,39 +102,59 @@ fn scramble(phrase: &str, ops: &[Operation]) -> String {
     chars.iter().collect()
 }
 
+/// Unscrambles a phrase using the supplied operations
+fn unscramble(phrase: &str, ops: &[Operation]) -> String {
+    let mut chars = phrase.chars().collect::<VecDeque<_>>();
+    for op in ops.iter().rev() {
+        // For each operation in reverse, undo what it would normally do
+        match op {
+            Operation::SwapPosition(y, x) => {
+                chars.swap(*x, *y);
+            }
+            Operation::SwapLetter(b, a) => {
+                let (x, y) = (
+                    chars.iter().position(|l| l == a).unwrap(),
+                    chars.iter().position(|l| l == b).unwrap(),
+                );
+                chars.swap(x, y);
+            }
+            Operation::RotateLeft(steps) => chars.rotate_right(*steps),
+            Operation::RotateRight(steps) => chars.rotate_left(*steps),
+            Operation::RotateBasedOnPositionOfLetter(a) => {
+                let pos = chars.iter().position(|l| l == a).unwrap();
+                // Determined by checking all possible rotate outputs
+                // Odds and 0 behave one way, evens the other
+                let rsteps = pos / 2 + if pos % 2 == 1 || pos == 0 { 1 } else { 5 };
+                chars.rotate_left(rsteps);
+            }
+            Operation::ReversePositions(x, y) => {
+                for (i, c) in (*x..=*y)
+                    .rev()
+                    .map(|i| chars[i])
+                    .collect::<Vec<_>>()
+                    .iter()
+                    .enumerate()
+                {
+                    chars[*x + i] = *c;
+                }
+            }
+            Operation::Move(y, x) => {
+                let value = chars.remove(*x).unwrap();
+                chars.insert(*y, value);
+            }
+        }
+    }
+    chars.iter().collect()
+}
+
 #[aoc(day21, part1)]
 fn part1(input: &[Operation]) -> String {
-    scramble(
-        if input.len() < 10 {
-            "abcde"
-        } else {
-            "abcdefgh"
-        },
-        input,
-    )
+    scramble("abcdefgh", input)
 }
 
 #[aoc(day21, part2)]
-fn part2(input: &[Operation]) -> Option<String> {
-    let target;
-    let letters;
-    if input.len() < 10 {
-        letters = "abcde";
-        target = "decab";
-    } else {
-        letters = "abcdefgh";
-        target = "fbgdceah";
-    }
-    for password in letters
-        .chars()
-        .permutations(letters.len())
-        .map(|chars| chars.iter().collect::<String>())
-    {
-        if target == scramble(&password, input) {
-            return Some(password);
-        }
-    }
-    None
+fn part2(input: &[Operation]) -> String {
+    unscramble("fbgdceah", input)
 }
 
 #[cfg(test)]
@@ -152,11 +175,23 @@ mod tests {
 
     #[test]
     fn test_part1_example() {
-        assert_eq!(part1(&gen(EXAMPLE_INPUT)), "decab");
+        assert_eq!(scramble("abcde", &gen(EXAMPLE_INPUT)), "decab");
     }
 
     #[test]
     fn test_part2_example() {
-        assert_eq!(part2(&gen(EXAMPLE_INPUT)), Some("abcde".to_owned()));
+        assert_eq!(unscramble("decab", &gen(EXAMPLE_INPUT)), "abcde");
+    }
+
+    #[test]
+    fn test_rotate_base_on_position() {
+        let phrase = "abcdefgh";
+        for c in phrase.chars() {
+            // For all chars, scramble and then unscramble to check rotating for all positions
+            let ops = [Operation::RotateBasedOnPositionOfLetter(c)];
+            let scrambled = scramble(phrase, &ops);
+            let unscrambled = unscramble(&scrambled, &ops);
+            assert_eq!(phrase, unscrambled);
+        }
     }
 }

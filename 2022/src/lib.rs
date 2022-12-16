b8c7eaea94056cbd905b882e2fa07d1a56d70a0a
Will Lunniss <wlunniss@rapitasystems.com>
2022-12-16 19:18:40 +0800

    Add initial project structure for 2022

---
+++ 2022/src/lib.rs
@@ -0,0 +1,10 @@
+#![warn(clippy::all, clippy::pedantic, clippy::nursery)]
+
+extern crate aoc_runner;
+
+#[macro_use]
+extern crate aoc_runner_derive;
+
+pub mod solutions;
+
+aoc_lib! { year = 2022 }
